5407bbd9427a6f61221a0b11927dc103dd8d421a
g0djan <g0dj4n@gmail.com>
2019-06-07 17:49:17 +0300

    triage

--- internal/backup_push_handler.go
+++ internal/backup_push_handler.go
@@ -105,9 +105,6 @@ func HandleBackupPush(uploader *Uploader, archiveDirectory string) {
 	uploader.UploadingFolder = basebackupFolder // TODO: AB: this subfolder switch look ugly. I think typed storage folders could be better (i.e. interface BasebackupStorageFolder, WalStorageFolder etc)
 
 	crypter := NewOpenPGPCrypter()
-	if crypter != nil {
-		crypter = nil
-	}
 	bundle := NewBundle(archiveDirectory, crypter, previousBackupSentinelDto.BackupStartLSN, previousBackupSentinelDto.Files)
 
 	var meta ExtendedMetadataDto
